decc9f50a25030ff350bcc9337e3048ed04a41eb
Maury M. Marques <maurymmarques@gmail.com>
2012-04-01 20:24:22 -0300

    Removed extends AppController and beforeFilter() method

--- Controller/MinifyController.php
+++ Controller/MinifyController.php
@@ -8,7 +8,7 @@ App::import('Minify.Vendor', 'minify' . DS . 'index');
  *
  * @package		app.Controller
  */
-class MinifyController extends AppController {
+class MinifyController extends Controller {
 
 	public $name = 'Minify';
 
@@ -20,17 +20,6 @@ class MinifyController extends AppController {
 	public function index() {
 		$this->response->statusCode('304');
 		exit;
-	}
-
-	/**
-	 * Checks if the Auth component is being used to give permission to the index() method.
-	 *
-	 * @return void
-	 */
-	public function beforeFilter() {
-		if (Set::check($this->Auth)) {
-			$this->Auth->allowedActions = array('index');
-		}
 	}
 
 }
