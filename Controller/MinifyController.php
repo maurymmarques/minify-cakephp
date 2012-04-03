bf4bf476fd26d042b878786bc780847810420d0d
Maury M. Marques <maurymmarques@gmail.com>
2012-04-03 02:14:40 -0300

    The import is not defined outside the class to avoid errors if the class is read from the console

--- Controller/MinifyController.php
+++ Controller/MinifyController.php
@@ -1,6 +1,4 @@
 <?php
-App::import('Minify.Vendor', 'minify' . DS . 'index');
-
 /**
  * Minify Controller
  *
@@ -14,10 +12,13 @@ class MinifyController extends Controller {
 
 	/**
 	 * Take care of any minifying requests.
+	 * The import is not defined outside the class to avoid errors if the class is read from the console.
 	 *
 	 * @return void
 	 */
 	public function index() {
+		App::import('Minify.Vendor', 'minify' . DS . 'index');
+
 		$this->response->statusCode('304');
 		exit;
 	}
