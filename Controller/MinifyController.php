abe5a5664f454eeb388fd7dd968de78ef73bc76c
Izik Dubnov <izikd@github>
2012-08-20 08:57:33 +0300

    Added Plugins support
    * Added support to serve css\js from Plugins webroot dirs
    * Changed the way the filenames represent at "f="
    * Fixed App::import to support CakePHP 2.2

--- Controller/MinifyController.php
+++ Controller/MinifyController.php
@@ -16,8 +16,40 @@ class MinifyController extends Controller {
 	 *
 	 * @return void
 	 */
-	public function index() {
-		App::import('Minify.Vendor', 'minify' . DS . 'index');
+	public function index($type) {
+		$files = array_unique(explode(',', $_GET['f']));
+		$plugins = array();
+		$pluginSymlinks = array();
+		$newFiles = array();
+		foreach ($files as &$file)
+		{
+		    if (empty($file))
+		    {
+			continue;
+		    }
+		    
+		    $plugin = false;
+		    list($first, $second) = pluginSplit($file);
+		    if (CakePlugin::loaded($first) === true) {
+			$file = $second;
+			$plugin = $first;
+		    }
+
+		    $pluginPath = (!empty($plugin) ? '..' . DS . 'Plugin' . DS . $plugin . DS . WEBROOT_DIR . DS : '');
+		    $file = $pluginPath . $type . DS . $file . '.' . $type;
+		    $newFiles[] = $file;
+
+		    if (!empty($plugin) && !isset($plugins[$plugin]))
+		    {
+			$plugins[$plugin] = true;
+
+			$pluginSymlinks['/' . $this->request->base . '/' . Inflector::underscore($plugin)] = APP . 'Plugin' . DS . $plugin . DS . WEBROOT_DIR;
+		    }
+		}
+		$_GET['f'] = implode(',', $newFiles);
+		$_GET['symlinks'] = $pluginSymlinks;
+
+		App::import('Vendor', 'Minify.minify/index');
 
 		$this->response->statusCode('304');
 		exit;
