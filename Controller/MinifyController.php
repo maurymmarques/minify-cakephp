ccf0fc0bfe1526282f73d2fb61ee94b8686f7532
lethal-guitar <lethal_guitar128@web.de>
2013-02-12 21:35:36 +0100

    Fix support for CakePHP not on base url
    File names are passed correctly to Minify when CakePHP is *not* deployed on the base url `/`.
    This restores the behavior provided by the version from SHA 1098e610c76491a989ba05f616ac325d455353d1.

--- Controller/MinifyController.php
+++ Controller/MinifyController.php
@@ -12,11 +12,26 @@ class MinifyController extends Controller {
  * @return void
  */
 	public function index($type) {
+		if (!empty($this->request->base)) {
+			$this->_adjustFilenames();
+		}
+
 		App::import('Vendor', 'Minify.minify/index');
 
 		$this->response->statusCode('304');
 		exit();
 	}
 
+	private function _adjustFilenames() {
+		$baseUrl = $this->request->base;
+		$baseLen = strlen($baseUrl);
+		$files = explode(',', $_GET['f']);
+		foreach ($files as &$file) {
+			if (!strncmp($file, $baseUrl, $baseLen)) {
+				$file = substr($file, $baseLen);
+			}
+		}
+		$_GET['f'] = implode(',', $files);
+	}
 }
 ?>
